1b5b52d7bc0d1aab74804d8021c4befd6280d06c
Vasliddin94 <vasliddin94@mail.ru>
2022-08-30 21:11:39 +0100

    sadas

---
+++ js/index.js
@@ -0,0 +1,19 @@
+ScrollReveal().reveal('div', {
+  duration:2000,
+  origin:'top',
+  distance:'100px',
+  scale:1.1
+})
+
+const showMenu=(toggleId, navId)=>{
+  const toggle=document.getElementById(toggleId),
+  nav=document.getElementById(navId)
+
+  if (toggle && nav) {
+    toggle.addEventListener('click', ()=>{
+      nav.classList.toggle('show-menu')
+    })
+  }
+}
+
+showMenu('nav-toggle', 'nav-menu')
